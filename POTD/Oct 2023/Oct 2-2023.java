85eb4528d16dc58afe2b8559bedc086b660bfc28
sourav2831 <sourav2831@gmail.com>
2023-10-02 11:31:49 +0530

    added oct 2 potd

---
+++ POTD/Oct 2023/Oct 2-2023.java
@@ -0,0 +1,24 @@
+//Number of distinct subsequences
+
+class Solution {
+    int distinctSubsequences(String str) {
+        int mod=(int)Math.pow(10,9)+7;
+        int n=str.length();
+        if(n==0) return 1;
+        HashMap<Character,Integer> map = new HashMap<>();
+        int dp[] = new int[n+1];
+        dp[0] = 1; dp[1]=2;
+        map.put(str.charAt(0),0);
+        for(int i=2; i<=n; i++){
+            char c=str.charAt(i-1);
+            dp[i]=(2*dp[i-1])%mod;
+            if(map.containsKey(c)){
+                int prevInd=map.get(c);
+                dp[i]=(dp[i]-dp[prevInd])%mod;
+                if(dp[i]<0) dp[i]+=mod;
+            }
+            map.put(c,i-1);
+        }
+        return dp[n];
+    }
+}
