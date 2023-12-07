06256a002cd3e97820bdd827a832347dc1e737fb
sourav2831 <sourav2831@gmail.com>
2023-12-07 18:32:27 +0530

    added dec 6 potd

---
+++ POTD/Dec 2023/Dec 6-2023.java
@@ -0,0 +1,15 @@
+//How Many X's?
+
+class Solution {
+    int countX(int L, int R, int X) {
+        int count = 0;
+        for(int i = L+1 ; i<R;i++) {
+            int n=i;
+            while(n!=0) {
+                if(n%10==X)count++;
+                n/=10;
+            }
+        }
+        return count;
+    }
+}
