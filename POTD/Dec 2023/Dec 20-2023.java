1b599cc1758209544e2df0b4e2338f2d749f7660
sourav2831 <sourav2831@gmail.com>
2023-12-20 00:32:59 +0530

    added dec 20 potd

---
+++ POTD/Dec 2023/Dec 20-2023.java
@@ -0,0 +1,15 @@
+//Modified Game of Nim
+
+class Solution {
+    static int findWinner(int n, int A[]) {
+        int xor = 0;
+        for (int num : A) {
+            xor ^= num;
+        }
+        if (xor == 0 || n % 2 == 0) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+}
