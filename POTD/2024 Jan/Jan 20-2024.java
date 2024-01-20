92eb7862b5cc43f34b8c55e12c560fc69dd8989d
sourav2831 <sourav2831@gmail.com>
2024-01-20 15:22:09 +0530

    added jan 20 potd

---
+++ POTD/2024 Jan/Jan 20-2024.java
@@ -0,0 +1,16 @@
+//Distribute candies in a binary tree
+
+class Solution{
+    public static int solve(Node root, int[] moves){
+        if(root == null)return 0;
+        int left = solve(root.left, moves);
+        int right = solve(root.right, moves);
+        moves[0] += Math.abs(left) + Math.abs(right);
+        return root.data + left + right - 1;
+    }
+    public static int distributeCandy(Node root){
+        int[] ans = new int[1];
+        solve(root, ans);
+        return ans[0];
+    }
+}
