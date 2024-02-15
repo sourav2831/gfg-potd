3f597e77f2b15494456bfbbab93d811e6c125bd2
sourav2831 <sourav2831@gmail.com>
2024-02-15 08:54:36 +0530

    added feb 15 potd

---
+++ POTD/2024 Feb/Feb 15-2024.java
@@ -0,0 +1,21 @@
+//Count all Possible Path
+
+class Solution
+{
+    public int isPossible(int[][] paths)
+    {
+        // Code here
+         for(int i=0;i<paths.length;i++)
+        {
+            int count = 0;
+            for(int j=0;j<paths.length;j++)
+            {
+                if(paths[i][j]==1)
+                    count++;
+            }
+            if(count%2==1) 
+                return 0;
+        }
+        return 1;
+    }
+}
