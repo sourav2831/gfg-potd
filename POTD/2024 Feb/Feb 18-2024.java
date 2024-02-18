2c5ef6019915f2054af241606a90fcfd65f8b20c
sourav2831 <sourav2831@gmail.com>
2024-02-18 17:40:59 +0530

    added feb 18 potd

---
+++ POTD/2024 Feb/Feb 18-2024.java
@@ -0,0 +1,11 @@
+//Sum of leaf nodes in BST
+
+class Solution
+{
+    public static int sumOfLeafNodes(Node root)
+    {
+        if(root==null)return 0;
+        if(root.left == null && root.right == null)return root.data;
+        return sumOfLeafNodes(root.left)+sumOfLeafNodes(root.right);
+    }
+}
