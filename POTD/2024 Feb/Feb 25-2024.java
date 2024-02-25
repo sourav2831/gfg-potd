453fa0d17ba086fedcc9a2cc6cf310f7294791d5
sourav2831 <sourav2831@gmail.com>
2024-02-25 16:26:56 +0530

    added feb 25 potd

---
+++ POTD/2024 Feb/Feb 25-2024.java
@@ -0,0 +1,22 @@
+//Reach a given score
+
+class Geeks {
+    public long count(int n) {
+        // Add your code here.
+        long[] dp= new long[(int)n+1];
+        dp[0]=1;
+        dp[1]=0;
+        dp[2]=0;
+        
+        for(int i =3;i<=n;i++){
+            dp[i]+=dp[i-3];
+        }
+        for(int i =5;i<=n;i++){
+            dp[i]+=dp[i-5];
+        }
+        for(int i =10;i<=n;i++){
+            dp[i]+=dp[i-10];
+        }
+        return dp[n];
+    }
+}
