e7800e50f587f1d51a1f8337b323d72378538aef
sourav2831 <sourav2831@gmail.com>
2024-05-18 10:52:54 +0530

    added may 18 potd

---
+++ POTD/2024 May/May 18-2024.java
@@ -0,0 +1,10 @@
+//Find the Highest number
+
+class Solution {
+    public int findPeakElement(List<Integer> a) {
+        // Code here
+        Collections.sort(a);
+        int size = a.size();
+        return a.get(size-1);
+    }
+}
