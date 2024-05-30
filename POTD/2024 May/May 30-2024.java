cfe9708e6bc921f0bb9dbbb946ed7812b960e584
sourav2831 <sourav2831@gmail.com>
2024-05-30 09:45:51 +0530

    added may 30 potd

---
+++ POTD/2024 May/May 30-2024.java
@@ -0,0 +1,26 @@
+//String Subsequence
+
+class Solution {
+    static final int MOD = (int)1e9 + 7;
+    public static int countWays(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % MOD;
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
