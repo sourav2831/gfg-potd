4fa8c4b50dd34ead0871fb0629279bef9b0d3536
sourav2831 <sourav2831@gmail.com>
2024-06-14 18:01:41 +0530

    added june 14 potd

---
+++ POTD/2024 June/June 14-2024.java
@@ -0,0 +1,21 @@
+//Armstrong Numbers
+
+class Solution {
+    static String armstrongNumber(int n) {
+        int sum =0;
+        int temp = n;
+        int a = n%10;
+        n = n/10;
+        sum+=a*a*a;
+        a = n%10;
+        n = n/10;
+        sum+=a*a*a;
+        a = n%10;
+        n = n/10;
+        sum+=a*a*a;
+        if(sum==temp){
+            return "true";
+        }
+        return "false";
+    }
+}
