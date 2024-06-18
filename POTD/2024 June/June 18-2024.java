c46ef3f01fa4426d1efe599d7530b9f2f5463ccf
sourav2831 <sourav2831@gmail.com>
2024-06-18 19:24:03 +0530

    added june 18 potd

---
+++ POTD/2024 June/June 18-2024.java
@@ -0,0 +1,15 @@
+//Number of Rectangles in a Circle
+
+class Solution {
+    int rectanglesInCircle(int r) {
+        int c = 0;
+        for (int i = 1; i < 2 * r; i++) {
+            int k = 1; 
+            while (4 * r * r >= i * i + k * k) {
+                c++; 
+                k++; 
+            }
+        }
+        return c; 
+    }
+};
