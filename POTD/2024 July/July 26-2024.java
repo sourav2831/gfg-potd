59994af1331171f9b2d09d8a0e865b764fc165e2
sourav2831 <sourav2831@gmail.com>
2024-07-26 19:19:10 +0530

    added july 26 potd

---
+++ POTD/2024 July/July 26-2024.java
@@ -0,0 +1,12 @@
+class Solution {
+    boolean kPangram(String str, int k) {
+        int alphaCount = 0;
+        int duplicatesCount = 0;
+        HashSet<Character> alpha = new HashSet<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (alpha.contains(str.charAt(i))) duplicatesCount += 1;
+            else if (str.charAt(i) != ' ') alpha.add(str.charAt(i));
+        }
+        return (alpha.size() == 26 || ((26 - alpha.size() <= duplicatesCount) && (26 - alpha.size()) <= k));
+    }
+}
