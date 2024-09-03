a549ba60f59f6e6bdafa0b819286c01ff8665954
sourav2831 <sourav2831@gmail.com>
2024-09-03 05:54:48 +0530

    added sept 3 potd

---
+++ POTD/2024 Sept/Sept 3-2024.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int lcs(String s1,String s2){
+        int n=s1.length();
+        int m=s2.length();
+        int [][]dp=new int[n+1][m+1];
+        for(int i=0;i<=n;i++) dp[i][0]=0;
+        for(int i=0;i<=m;i++) dp[0][i]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
+    }
+	public int minOperations(String str1, String str2){
+	    int n=str1.length();
+        int m=str2.length();
+        int lcs=lcs(str1,str2);
+        if(n>m){
+            if(lcs==m) return(n-lcs);
+            else return (n-lcs+Math.abs(lcs-m));
+        }
+        else return (n-lcs+Math.abs(lcs-m));
+	} 
+}
