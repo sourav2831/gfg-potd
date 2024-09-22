78da1c2b93e429539efcf958d8e67e309e63e7ca
sourav2831 <sourav2831@gmail.com>
2024-09-22 18:59:20 +0530

    added sept 22 potd

---
+++ POTD/2024 Sept/Sept 22-2024.java
@@ -0,0 +1,24 @@
+class Solution {
+    int lps(String s) {
+        int n=s.length();
+        int lps[]=new int[n];
+        lps[0]=0;
+        int len=0;
+        int i=1;
+        while(i<n){
+            if(s.charAt(i)==s.charAt(len)){
+                len++;
+                lps[i]=len;
+                i++;
+            }else{
+                if(len!=0){
+                    len=lps[len-1];
+                }else{
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+        return lps[n-1];
+    }
+}
