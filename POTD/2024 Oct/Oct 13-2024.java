1b1bea0352bb51f5320a8af5a8697d434bafcf74
sourav2831 <sourav2831@gmail.com>
2024-10-13 20:59:41 +0530

    added oct 13 potd

---
+++ POTD/2024 Oct/Oct 13-2024.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void deleteAlt(Node head) {
+        Node temp=head; 
+        while(temp!=null && temp.next!=null){
+            temp.next=temp.next.next;
+            temp=temp.next;
+        }
+    }
+}
