eab617bc99ef844662decd4c7d9fdb06be387e5b
sourav2831 <sourav2831@gmail.com>
2024-11-02 18:40:22 +0530

    added nov 2 potd

---
+++ POTD/2024 Nov/Nov 2-2024.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean checkDuplicatesWithinK(int[] arr, int k) {
+        HashMap<Integer,Integer> map =new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i])){
+                if(i-map.get(arr[i])<=k) return true;
+            }
+            else map.put(arr[i],i);
+        }
+        return false;
+    }
+}
