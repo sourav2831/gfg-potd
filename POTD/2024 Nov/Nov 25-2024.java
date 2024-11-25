239ac4a9878e9ab5707b3ecb650ea70bffcdb137
sourav2831 <sourav2831@gmail.com>
2024-11-25 22:19:07 +0530

    added nov 25 potd

---
+++ POTD/2024 Nov/Nov 25-2024.java
@@ -0,0 +1,26 @@
+class Solution {
+    int maxProduct(int[] arr) {
+        int n = arr.length;
+        int maxlr = Integer.MIN_VALUE,maxrl = Integer.MIN_VALUE;
+        int curr1 = 1,curr2 = 1;
+        for(int i=0;i<n;i++){
+            if(arr[i] == 0){
+                curr1 = 1;
+                continue;
+            }
+            curr1 *= arr[i];
+            maxlr = Math.max(maxlr,curr1);
+        }
+        for(int i=n-1;i>=0;i--){
+            if(arr[i] == 0){
+                curr2 = 1;
+                continue;
+            }
+            curr2 *= arr[i];
+            maxrl = Math.max(maxrl,curr2);
+        }
+        if(n==1) return arr[0];
+        if(maxlr<0 && maxrl<0) return 0;
+        return Math.max(maxlr,maxrl);
+    }
+}
