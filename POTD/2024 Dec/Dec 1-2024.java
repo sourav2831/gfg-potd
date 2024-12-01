ae062ea40375e4a9dbc0b0f6ac46f0f8f5f43446
sourav2831 <sourav2831@gmail.com>
2024-12-01 19:19:46 +0530

    added dec 1 potd

---
+++ POTD/2024 Dec/Dec 1-2024.java
@@ -0,0 +1,12 @@
+class Solution {
+    static char nonRepeatingChar(String s) {
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        for(char c : s.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for(char key : map.keySet()){
+            if(map.get(key) == 1)return key;
+        }
+        return '$';
+    }
+}
