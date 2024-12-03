bbb7a34ea4852151319258d7b3569217a5000ca7
sourav2831 <sourav2831@gmail.com>
2024-12-03 21:58:14 +0530

    added dec 3 potd

---
+++ POTD/2024 Dec/Dec 3-2024.java
@@ -0,0 +1,31 @@
+class Solution {
+    public static int minChar(String s) {
+        int n = s.length();
+        String rev_s = new StringBuilder(s).reverse().toString();
+        String combined = s + "#" + rev_s;
+        int[] lps = computeLPS(combined);
+        return n - lps[combined.length() - 1];
+    }
+
+    private static int[] computeLPS(String str) {
+        int n = str.length();
+        int[] lps = new int[n];
+        int len = 0;
+        int i = 1;
+        while (i < n) {
+            if (str.charAt(i) == str.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+}
