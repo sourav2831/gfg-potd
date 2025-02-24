ed4f85c2c7c51d3518343ad843a47d277740b750
sourav2831 <sourav2831@gmail.com>
2025-02-24 21:26:02 +0530

    added feb 24 potd

---
+++ POTD/2025 Feb/Feb 24-2025.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ArrayList<Integer> calculateSpan(int[] arr) {
+        // write code here
+        ArrayList<Integer> list = new  ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+        int l=arr.length;
+        for(int i=0;i<l;i++){
+            while(!st.isEmpty() && arr[st.peek()]<=arr[i])st.pop();
+           
+            if(!st.isEmpty())list.add(i-st.peek());
+            else list.add(i+1);
+            
+            st.push(i);        
+        }
+        return list;
+    }
+}
