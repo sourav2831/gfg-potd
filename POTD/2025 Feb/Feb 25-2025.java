ce200fd47ef7c7b615ca79aca460a347ebb4c5a8
sourav2831 <sourav2831@gmail.com>
2025-02-25 05:42:57 +0530

    added feb 25 potd

---
+++ POTD/2025 Feb/Feb 25-2025.java
@@ -0,0 +1,21 @@
+class Solution {
+    // Function to find largest rectangular area possible in a given histogram.
+    public static int getMaxArea(int arr[]) {
+        // your code here
+        Stack<Integer> s = new Stack<>();
+        int n = arr.length;
+        int ans = 0;
+        
+        for(int i=0;i<=n;i++){
+            int c = (i==n) ? 0 : arr[i];
+            
+            while(!s.isEmpty() && c<arr[s.peek()]){
+                int h = arr[s.pop()];
+                int w = (s.isEmpty()) ? i : (i-s.peek()-1);
+                ans = Math.max(ans, h*w );
+            }
+            s.push(i);
+        }
+        return ans;
+    }
+}
