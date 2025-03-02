755218ab441eda9bd0928723839927fb6282584b
sourav2831 <sourav2831@gmail.com>
2025-03-02 21:19:20 +0530

    added mar 2 potd

---
+++ POTD/2025 Mar/Mar 2-2025.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ArrayList<Integer> maxOfSubarrays(int arr[], int k) {
+        // code here
+        Deque<Integer> dq = new ArrayDeque<>();
+        int n = arr.length;
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i =0; i < n; i++) {
+            while(dq.size() != 0 && arr[dq.getLast()] < arr[i]) {
+                dq.removeLast();
+            }
+            dq.addLast(i);
+            if(i - dq.getFirst() == k) dq.removeFirst();
+            if(i >= k -1) {
+                ans.add(arr[dq.getFirst()]);
+            }
+        }
+        return ans;
+    }
+}
