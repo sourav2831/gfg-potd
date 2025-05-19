516b8b4cc7a0bb32b34ab8121dbe08dd895e65f5
sourav2831 <sourav2831@gmail.com>
2025-05-19 21:14:42 +0530

    added may 19 potd

---
+++ POTD/2025 May/May 19-2025.java
@@ -0,0 +1,37 @@
+class Solution {
+    public ArrayList<Node> findPreSuc(Node root, int key) {
+        // code here
+        ArrayList<Node> result = new ArrayList<>();
+        Node predecessor = null;
+        Node successor = null;
+        while (root != null) {
+            if (root.data == key) {
+                if (root.left != null) {
+                    Node temp = root.left;
+                    while (temp.right != null) {
+                        temp = temp.right;
+                    }
+                    predecessor = temp;
+                }
+                if (root.right != null) {
+                    Node temp = root.right;
+                    while (temp.left != null) {
+                        temp = temp.left;
+                    }
+                    successor = temp;
+                }
+                break;
+            } else if (root.data < key) {
+                predecessor = root;
+                root = root.right;
+            } else {
+                successor = root;
+                root = root.left;
+            }
+        }
+        result.add(predecessor);
+        result.add(successor);
+        return result;
+    
+    }
+}
