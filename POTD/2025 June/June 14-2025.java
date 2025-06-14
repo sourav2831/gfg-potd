d613f121f2c09203f9275ba676007db0ad62bce0
sourav2831 <sourav2831@gmail.com>
2025-06-14 21:53:44 +0530

    added june 14 potd

---
+++ POTD/2025 June/June 14-2025.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isSymmetric(Node root) {
+        // Code here
+        
+         return fun(root, root);
+    }
+    public boolean fun(Node l , Node r){
+        if( l == null && r == null) return true;
+        if(l == null || r == null) return false;
+        
+        
+        boolean ans =  fun(l.left,r.right) && fun(l.right,r.left);
+       if(l.left != null &&  r.right != null) 
+       ans = ans && (l.left.data == r.right.data); 
+       if(r.left != null && l.right != null)
+       ans = ans && (l.right.data ==
+        r.left.data);
+        
+        return ans;
+    }
+}
+
