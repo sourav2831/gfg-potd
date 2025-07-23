61490c2392e759cc2ede1dff1941417fe2e62da7
sourav2831 <sourav2831@gmail.com>
2025-07-23 21:50:07 +0530

    added july 23 potd

---
+++ POTD/2025 July/July 23-2025.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int subarraySum(int[] arr) 
+    {
+        int len = arr.length;
+        int sum  = 0;
+        for(int i=0;i<len;i++)
+        {
+            sum+=arr[i]*(i+1)*(len-i);
+        }
+        return sum;
+        
+    }
+}
