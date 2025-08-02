af2b3f4e55da4148dd2e4c5f8d923c8f7fe3333c
sourav2831 <sourav2831@gmail.com>
2025-08-02 20:39:30 +0530

    added aug 2 potd

---
+++ POTD/2025 August/Aug 2-2025.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestSubarray(int[] arr, int k) {
+        int n = arr.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        int cnt = 0, ans = 0;
+        for (int i = 0; i < n; i++) {
+            cnt += (arr[i] > k) ? 1 : -1;
+            if (cnt > 0) {
+                ans = i + 1;
+            } else {
+                if (map.containsKey(cnt - 1)) {
+                    ans = Math.max(ans, i - map.get(cnt - 1));
+                }
+            }
+            map.putIfAbsent(cnt, i);
+        }
+        return ans;
+    }
+}
