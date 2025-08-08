65771be4f80d3404bb3c94d873e582d1f9efab45
sourav2831 <sourav2831@gmail.com>
2025-08-08 22:27:44 +0530

    added aug 8 potd

---
+++ POTD/2025 August/Aug 8-2025.java
@@ -0,0 +1,25 @@
+class Solution {
+    int getLPSLength(String s) {
+        // code here
+        int n=s.length();
+        int p=0;
+        int su=1;
+        int count=0;
+        int pos=1;
+        while(p<n && su<n){
+            if(s.charAt(p)==s.charAt(su)){
+                p++;
+                su++;
+                count++;
+            }
+            else{
+                p=0;
+                count=0;
+                pos++;
+                su=pos;
+            }
+        }
+        return count;
+    }
+} 
+
